4c61375793133d1713784425f1818658b1fec1ce
Jimmy Sherpa <jimmy483>
2022-03-20 18:17:40 +0545

    completed?

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
  String[][] pointer=new String[3][3];
@@ -9,6 +6,10 @@ public class Main {
  boolean cont=false;
  boolean gameFinished=false;
  List<Integer> cpuPossibleMoves=new ArrayList<>();
+ boolean userWin=false;
+ boolean cpuWin=false;
+// List<Integer> cpuMoves=new ArrayList<>();
+// List<Integer> userMoves=new ArrayList<>();
  public Main(){
      program();
     }
@@ -25,21 +26,58 @@ public class Main {
         System.out.println("Note:The room at the top left is number 1 and bottom right is number 9");
         //int count=0;
         do {
-            while(!gameFinished){
+            while(!gameFinished&&!cpuWin&&!userWin){
                 System.out.println("Please select a room number to tick(1-9)");
                 Scanner sc=new Scanner(System.in);
+                while (!sc.hasNextInt()){
+                    System.out.println("Pease enter a valid room number");
+                    sc.next();
+                }
                 int choice=sc.nextInt();
+                if(choice>9||choice<1){
+                    System.out.println("Invalid Number");
+                    System.out.println("Ending program");
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println("3");
+                        Thread.sleep(1000);
+                        System.out.println("2");
+                        Thread.sleep(1000);
+                        System.out.println("1");
+                        Thread.sleep(1000);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return;
+                }
                 tick(choice,"X");
+                //userMoves.add(choice);
                 gameFinished=cpuMove();
                 System.out.println(cpuPossibleMoves);
-                if(cpuPossibleMoves.size()>0){
+                userWin=checkWin("X");
+                if(userWin){
+                    System.out.println("You Win");
+                    //showBoard();
+                }
+                if(cpuPossibleMoves.size()>0 && !userWin){
                     Random rn=new Random();
                     int cpuTick=rn.nextInt(cpuPossibleMoves.size());
                     tick(cpuPossibleMoves.get(cpuTick),"O");
+                    cpuWin=checkWin("O");
+                    //cpuMoves.add(cpuPossibleMoves.get(cpuTick));
                 }
+                if(cpuWin){
+                    System.out.println("You Lose");
+                    //showBoard();
+                }
+
                 showBoard();
                 //count++;
 
+            }
+            if(gameFinished){
+                System.out.println("Game was a tie");
             }
             System.out.println("New Game?(Y/N)");
             Scanner sc=new Scanner(System.in);
@@ -48,6 +86,8 @@ public class Main {
                 cont=true;
             }
             cpuPossibleMoves.clear();
+            cpuWin=false;
+            userWin=false;
             gameFinished=false;
             clearBoard();
         }while (cont);
@@ -99,6 +139,44 @@ public class Main {
                 pointer[i][j]=" ";
             }
         }
+    }
+
+
+    public boolean checkWin(String mark){
+     //win lines
+        //1,2,3  4,5,6   7,8,9   1,4,7   2,5,8   3,6,9   1,5,9  3,5,7
+//        for(int i=0;i< pointer.length;i++){
+//            for(int j=0;i< pointer.length;j++){
+//                if(pointer[i][j].equals(mark)){
+//
+//                }
+//            }
+//
+//        }
+        boolean win=false;
+        if(!mark.equals("")){
+            if(pointer[0][0].equals(mark) && pointer[0][1].equals(mark) && pointer[0][2]==mark){
+                win=true;
+            }else if(pointer[1][0].equals(mark) && pointer[1][1].equals(mark) && pointer[1][2]==mark){
+                win=true;
+            }else if(pointer[2][0].equals(mark) && pointer[2][1].equals(mark) && pointer[2][2]==mark){
+                win=true;
+            }else if(pointer[0][0].equals(mark) && pointer[1][0].equals(mark) && pointer[2][0]==mark){
+                win=true;
+            }else if(pointer[0][1].equals(mark) && pointer[1][1].equals(mark) && pointer[2][1]==mark){
+                win=true;
+            }else if(pointer[0][2].equals(mark) && pointer[1][2].equals(mark) && pointer[2][2]==mark){
+                win=true;
+            }else if(pointer[0][0].equals(mark) && pointer[1][1].equals(mark) && pointer[2][2]==mark){
+                win=true;
+            }else if(pointer[0][2].equals(mark) && pointer[1][1].equals(mark) && pointer[2][0]==mark){
+                win=true;
+            }
+        }
+
+        return win;
+
+
     }
 
     public boolean cpuMove(){
